b975c0ea20bb4bd8ed9ab3bd5caa928d0f268327
anangyoga <aharwinto@gmail.com>
2023-06-29 11:11:53 +0700

    fix: Navbar hover scale x

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,8 +8,9 @@ const menus = ["Home", "Delivery", "Pricing", "FAQs", "Contact"];
     <a href="#" class="text-4xl font-bold tracking-wide">Oregan<span class="text-green">o</span></a>
 
     <ul class="flex items-center space-x-8">
-      <li class="text-xl font-medium" :class="{ 'text-green': activeMenu === menu }" v-for="menu in menus">
+      <li class="text-xl font-medium group" :class="{ 'text-green': activeMenu === menu }" v-for="menu in menus">
         <a href="#"> {{ menu }} </a>
+        <div class="h-0.5 bg-green scale-x-0 group-hover:scale-100 transition-transform origin-left rounded-full duration-300 ease-out"></div>
       </li>
     </ul>
   </nav>
