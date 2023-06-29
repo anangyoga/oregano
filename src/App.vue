b2e73506e6e1015ba71e540fc3712a596aa8cbc2
anangyoga <aharwinto@gmail.com>
2023-06-29 21:56:28 +0700

    fix: blur background Homepage

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <div class="w-full min-h-screen font-sans text-gray-900">
+  <div class="w-full min-h-screen font-sans text-gray-900 bg-gradient-to-br from-transparent to-green-100">
     <Navbar />
     <Header />
   </div>
