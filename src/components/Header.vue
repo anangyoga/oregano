fa4abca13f1f3e59be09906fcc53e123cf0e225f
anangyoga <aharwinto@gmail.com>
2023-06-29 22:50:58 +0700

    fix: responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,15 @@ import StarIcon from "../assets/star.svg";
 import FuelerIcon from "../assets/fueler.svg";
 </script>
 <template>
-  <div class="flex justify-between px-24 py-8 max-w-screen-xl mx-auto">
-    <div class="max-w-md relative">
-      <img class="w-24 h-auto absolute -top-4 -right-16" src="/img/leaf.png" alt="leaf" />
+  <div class="flex flex-wrap-reverse justify-between gap-y-24 py-12 px-6 sm:px-8 md:px-12 lg:px-16 xl:px-24 max-w-screen-xl mx-auto">
+    <div class="relative md:w-1/2 w-full">
+      <img class="absolute top-0 right-0 md:-top-4 md:-right-8 w-24 h-auto" src="/img/leaf.png" alt="leaf" />
       <span class="flex items-center px-1 text-xl text-green">
         <span class="font-medium">100% Organic food</span>
         <img class="h-8 w-auto" src="/img/vegetable.png" alt="vegetable" />
       </span>
-      <h1 class="pt-4 leading-tight text-6xl font-bold tracking-tighter">Healthy Lifestyle is your only <span class="text-green whitespace-nowrap">unfair advantage</span></h1>
-      <p class="pt-8 text-lg font-normal text-gray-600 leading-relaxed">Choose healthy habits with Oregano daily meal prepared by our expert nutritionist and chef.</p>
+      <h1 class="pt-4 text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight">Healthy Lifestyle is your only <span class="text-green whitespace-nowrap">unfair advantage</span></h1>
+      <p class="pt-8 sm:text-lg max-w-md font-normal text-gray-600 leading-relaxed">Choose healthy habits with Oregano daily meal prepared by our expert nutritionist and chef.</p>
       <div class="flex space-x-6 pt-8">
         <button class="flex justify-center items-center bg-green text-white font-medium h-13 px-8 rounded-xl hover:shadow-primary transition-shadow duration-300 ease-out">Get Started</button>
         <button class="flex justify-center items-center font-medium h-13 px-8 rounded-xl ring-1 ring-black hover:shadow-2xl transition-shadow duration-300">Explore Menu</button>
@@ -26,8 +26,8 @@ import FuelerIcon from "../assets/fueler.svg";
       </div>
     </div>
     <div class="relative flex-1 flex flex-col justify-between">
-      <img class="max-w-xl drop-shadow-2xl self-end" src="img/dish.png" alt="dish" />
-      <div class="absolute -right-6 top-32 flex flex-col py-5 px-7 rounded-xl shadow-xl bg-white/80 backdrop-blur-xl hover:-translate-y-2 hover:shadow-xl cursor-pointer transition-all duration-300">
+      <img class="max-w-xl w-80 lg:w-full drop-shadow-2xl self-end" src="img/dish.png" alt="dish" />
+      <div class="absolute right-0 lg:-right-6 top-0 lg:top-28 flex flex-col py-5 px-7 rounded-2xl shadow-xl bg-white/80 backdrop-blur-xl hover:-translate-y-2 hover:shadow-2xl transition-all duration-300">
         <div class="flex -space-x-3">
           <div v-for="i in 3" class="w-13 h-13 rounded-full border-4 border-white object-cover overflow-hidden">
             <img :src="`/public/img/avatar-${i}.jpeg`" alt="" />
@@ -39,14 +39,14 @@ import FuelerIcon from "../assets/fueler.svg";
           <p>4.9 (+2.5K Ratings)</p>
         </div>
       </div>
-      <div class="absolute left-0 bottom-16 flex rounded-xl shadow-xl bg-white/80 backdrop-blur-xl hover:-translate-y-2 hover:shadow-xl cursor-pointer transition-all duration-300">
+      <div class="absolute left-0 bottom-0 md:bottom-32 lg:bottom-16 flex bg-white/80 rounded-2xl shadow-xl backdrop-blur-xl hover:-translate-y-2 hover:shadow-2xl transition-all duration-300">
         <img class="w-auto h-20 self-end" src="/img/driver.png" alt="driver" />
         <div class="pr-7 pl-2 py-5">
           <h3 class="font-bold">Fast Delivery</h3>
           <p class="text-gray-600 leading-relaxed">30 mins delivery</p>
         </div>
       </div>
-      <div class="flex justify-end space-x-1 font-bold">
+      <div class="hidden md:flex justify-end space-x-1 font-bold">
         <span>Powered by</span>
         <FuelerIcon class="w-6 h-6 text-gray-900 fill-current" />
         <span>Fueler</span>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,14 @@
 <script setup>
 const activeMenu = "Home";
 const menus = ["Home", "Delivery", "Pricing", "FAQs", "Contact"];
+import HamburgerIcon from "../assets/hamburger.svg";
 </script>
 
 <template>
-  <nav class="flex items-center justify-between px-24 py-8 max-w-screen-xl mx-auto">
+  <nav class="flex items-center justify-between px-6 py-8 sm:px-24 sm:py-8 max-w-screen-xl mx-auto">
     <a href="#" class="text-4xl font-bold tracking-wide">Oregan<span class="text-green">o</span></a>
 
-    <div class="flex items-center space-x-14">
+    <div class="hidden md:flex md:items-center md:space-x-14">
       <ul class="flex items-center space-x-8">
         <li class="text-xl font-medium group" :class="{ 'text-green': activeMenu === menu }" v-for="menu in menus">
           <a href="#"> {{ menu }} </a>
@@ -17,5 +18,8 @@ const menus = ["Home", "Delivery", "Pricing", "FAQs", "Contact"];
 
       <button class="flex items-center justify-center h-13 bg-green px-7 font-medium text-white rounded-xl hover:shadow-primary transition-shadow duration-300">Get Started</button>
     </div>
+    <button class="block md:hidden">
+      <HamburgerIcon class="w-8 h-8 fill-current text-gray-900" />
+    </button>
   </nav>
 </template>
